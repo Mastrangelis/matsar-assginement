f19ddb622d2a9f926b1997a685b02d33ed1a31cc
Nikos Mastrangelis <n.mastrangelis.ext@tradias.com>
2023-02-13 11:43:53 +0200

    fix

--- api/axios.ts
+++ api/axios.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    ...(process.env.NODE_ENV === 'development' && {
-        baseURL: 'http://localhost:3000'
-    }),
+    baseURL:
+        process.env.NODE_ENV === 'development'
+            ? 'http://localhost:3000'
+            : 'https://matsar-assginement-zbfj-git-deployment-mastrangelis.vercel.app/',
     withCredentials: false
 });
 
