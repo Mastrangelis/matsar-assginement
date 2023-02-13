aa43331e5b0e74635f35a001106a846b6058287d
Nikos Mastrangelis <n.mastrangelis.ext@tradias.com>
2023-02-13 11:18:25 +0200

    url fix

--- constants/url.ts
+++ constants/url.ts
@@ -3,6 +3,6 @@ const CLOUDFRONT_URL = 'https://d1ax460061ulao.cloudfront.net';
 const BASE_URL =
     process.env.NODE_ENV === 'development'
         ? 'http://localhost:3000/api'
-        : 'https://matsar-assginement-git-main-mastrangelis.vercel.app/api';
+        : 'https://api.matspar.se';
 
 export { CLOUDFRONT_URL, BASE_URL };
